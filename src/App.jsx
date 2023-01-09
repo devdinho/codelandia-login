561537ca2b977f5c6232afcfaf7aff6f827a500f
Diego Figueirêdo <8kball@gmail.com>
2023-01-09 10:04:12 -0300

    feat: add formcontainer with base style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import FormContainer from "./components/FormContainer";
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <FormContainer />
     </div>
   )
 }
---
+++ src/components/FormContainer/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const FormContainer = ({ children }) => {
+  return (
+    <div className="form-container">{ children }</div>
+  )
+};
+
+export default FormContainer;
